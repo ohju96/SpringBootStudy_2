d0a6b08e6636bca3b4bc67ab6e6fb8cd45c41eb1
ohju96 <ohju96@naver.com>
2022-03-29 14:20:33 +0900

    기존 프로젝트 삭제 및 README 수정

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SpringBootStudy_2
-학교 스프링 부트 수업 코드 저장소
+Melon 차트 검색 프로젝트
--- melon/melon/src/main/java/Project/melon/dto/MelonDTO.java
+++ melon/melon/src/main/java/Project/melon/dto/MelonDTO.java
@@ -3,14 +3,21 @@ package Project.melon.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+/**
+ * jackson 라이브러리는 int와 같은 숫자 데이터타입에
+ * 저장되지 않아 발생하는 null 오류를 방지하기 위해 값이 없다면
+ * 0으로 저장한다.
+ * Include.NON_NULL은 NULL인 것만 방지하기 때문에 숫자 타입은 출력되는 문제가 있다.
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class MelonDTO {
 
+
     String collectTime; // 수집 시간
     String song; // 노래 제목
     String singer; // 가수
-    int singerCnt;
+    int singerCnt; //차트에 등록된 가수별 노래 수
 
 
 }
